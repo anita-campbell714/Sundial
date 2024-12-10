32eaf944d0618ceb0bb3ddd713acf39604f064f3
NeetzBeatz <anitaleahcampbell@hotmail.co.uk>
2024-12-10 00:05:39 +0000

    adding word-editing features to notepad

--- index.js
+++ index.js
@@ -161,4 +161,4 @@ y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
 
-document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
